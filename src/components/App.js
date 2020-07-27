ab85f6c3c04e77905cd5ab35bde5085bee4375c7
koharakouki <ko.kohara02@gmail.com>
2020-07-27 17:29:26 +0900

    データ表示まで

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,32 @@ import Form from './Form';
 import List from './List';
 
 export default class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      todo: []
+    };
+    this.handleAdd = this.handleAdd.bind(this);
+  }
+
+  handleAdd(e){
+    e.preventDefault();
+
+    this.state.todo.push({ title: e.target.title.value });
+
+    this.setState({ todo: this.state.todo });
+
+    e.target.title.value = '';
+  }
+
   render(){
     return(
       <div className="siimple-box siimple--bg-dark">
         <h1 className="siimple-box-title siimple--color-white">React Todo App</h1>
-        <Form />
+        <Form handleAdd={this.handleAdd} />
         <div className="siimple-rule"></div>
-        <List />
+        <List todos={this.state.todo} />
       </div>
     );
   }
