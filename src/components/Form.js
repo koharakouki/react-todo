a30193a8b0c386b6cbbced784ace5a29d17c8ab1
koharakouki <ko.kohara02@gmail.com>
2020-07-28 17:31:11 +0900

    add formChenck

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
 
 const Form = (props) => (
+
+  constructor(props) {
+    super(props);
+    this.formCheck = this.formCheck.bind(this);
+  }
+
+  formCheck(e){
+    console.log(e.target.title.value);
+  }
+
   <form className="siimple-form" onSubmit={props.handleAdd} >
     <div className="siimple-form-field">
       <label className="siimple-label siimple--color-white">Your todo:</label>
-      <input name="title" type="text" className="siimple-input" />
+      <input name="title" type="text" className="siimple-input" onChange={this.formCheck} />
       <input type="submit" value="Add" className="siimple-btn siimple-btn--teal" />
     </div>
   </form>
